d7cddaf6e5d6d103c2e4edbaebc032f5ba753c92
Thomas-Adrian-Soler-Nilsson <solerthomasadrian@gmail.com>
2025-09-18 15:47:16 -0300

    Novo EX

---
+++ Atividade3Ds/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade3Ds
+{
+    internal class Pessoa
+    {
+        
+            public double Altura;
+            public string Sexo;
+
+
+            public double PesoIdeal()
+            {
+                double pesoIdeal;
+
+                // Verifica o sexo da pessoa para aplicar a fórmula correta
+                if (Sexo.ToLower(System.Globalization.CultureInfo.CurrentCulture) == "homem")
+                {
+                    pesoIdeal = (72.7 * Altura) - 58;
+                }
+                else if (Sexo.ToLower(System.Globalization.CultureInfo.CurrentCulture) == "mulher")
+                {
+                    pesoIdeal = (62.1 * Altura) - 44.7;
+                }
+                else
+                {
+                    throw new Exception("Sexo inválido. Informe 'homem' ou 'mulher'.");
+                }
+
+                return pesoIdeal;
+            }
+    }
+}
---
+++ Atividade3Ds/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace Atividade3Ds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa x, y;
+            x = new Pessoa();
+            y = new Pessoa();
+
+            // Entrada para o primeiro usuário (Pessoa X)
+            Console.WriteLine("Entre com os dados da pessoa X");
+            Console.Write("Altura: ");
+            x.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Sexo (homem/mulher): ");
+            x.Sexo = Console.ReadLine();
+
+            // Entrada para o segundo usuário (Pessoa Y)
+            Console.WriteLine("Entre com os dados da pessoa Y");
+            Console.Write("Altura: ");
+            y.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Sexo (homem/mulher): ");
+            y.Sexo = Console.ReadLine();
+
+            // Cálculo do peso ideal
+            double pesoX = x.PesoIdeal();
+            double pesoY = y.PesoIdeal();
+
+            // Exibição dos resultados
+            Console.WriteLine($"Peso ideal de X = {pesoX:F2} kg");
+            Console.WriteLine($"Peso ideal de Y = {pesoY:F2} kg");
+
+            // Comparação entre os pesos ideais
+            if (pesoX > pesoY)
+            {
+                Console.WriteLine("Maior peso ideal: X");
+            }
+            else if (pesoY > pesoX)
+            {
+                Console.WriteLine("Maior peso ideal: Y");
+            }
+            else
+            {
+                Console.WriteLine("Os pesos ideais são iguais.");
+            }
+        }
+    }
+}
